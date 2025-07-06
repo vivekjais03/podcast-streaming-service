83933081b96c714b36373e3a924d54df5460f728
Vivek Jaiswal <jaiswalvivek421@gmail.com>
2025-07-06 16:17:20 +0530

    Add podcast model file

--- server/models/Podcast.js
+++ server/models/Podcast.js
@@ -1,15 +1,1 @@
-// server/models/Podcast.js
-const mongoose = require("mongoose");
-
-const podcastSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    description: { type: String },
-    media: { type: String, required: true },
-    creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  },
-  { timestamps: true }
-);
-
-// ✅ Only define model if not already defined
-module.exports = mongoose.models.Podcast || mongoose.model("Podcast", podcastSchema);
+ 
